7821f587f82f9133bad82e7be06e6199e25e9ccc
Clint Baker <clintjbaker@gmail.com>
2024-03-11 14:34:15 -0600

    transition to hero

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <div className="h-screen flex justify-center items-center bg-gray-100">
       <div className="bg-gray-100 text-black text-center p-10 lg:p-20">
-        <h1 className="text-3xl lg:text-5xl font-bold mb-4">
+        <h1 className="text-3xl lg:text-5xl font-bold mb-4 transition-all duration-700 ease-in">
           Hey, my name is Clint Baker
         </h1>
         <p className="text-xl mb-0">I'm a full stack software developer.</p>
